47f7acb5b6bda2930d0ff05133e9c58dc3e80201
Alexander Bayandin <a.bayandin@gmail.com>
2013-09-09 16:03:04 +0700

    Add refresh to page

--- contesto/basis/page.py
+++ contesto/basis/page.py
@@ -17,6 +17,9 @@ class BasePage(object):
     def back(self):
         self._driver.back()
 
+    def refresh(self):
+        self._driver.refresh()
+
     @property
     def url(self):
         return self._driver.current_url
