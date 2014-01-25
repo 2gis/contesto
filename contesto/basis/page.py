b6da4020ef895d037fd3de71ef4224a7773ad45a
shiron <p.stashevsky@2gis.ru>
2014-01-25 14:54:16 +0700

    add page, webpage, iospage

--- contesto/basis/page.py
+++ contesto/basis/page.py
@@ -1,9 +1,27 @@
-class BasePage(object):
+from selenium.webdriver import TouchActions
+
+
+class Page(object):
+    """
+    Base class for Page. The main of Page is encapsulate page elements
+    for mobile, web or desktop application.
+    """
+
     def __init__(self, driver):
         """
         :type driver: ContestoDriver
         """
         self._driver = driver
+
+    def get_source(self):
+        return self._driver.page_source
+
+
+class WebPage(Page):
+
+    @property
+    def url(self):
+        return self._driver.current_url
 
     def get(self, url):
         """
@@ -11,15 +29,17 @@ class BasePage(object):
         """
         self._driver.get(url)
 
+    def refresh(self):
+        self._driver.refresh()
+
     def forward(self):
         self._driver.forward()
 
     def back(self):
         self._driver.back()
 
-    def refresh(self):
-        self._driver.refresh()
 
-    @property
-    def url(self):
-        return self._driver.current_url
+class IosScreen(Page):
+
+    def scroll_page(self, x, y):
+        TouchActions(self._driver).flick(x,y).perform()
---
+++ tests/page_tests.py
@@ -0,0 +1,20 @@
+from mock import Mock
+from contesto.basis.page import WebPage, Page
+import unittest
+
+class PageTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = Mock()
+        self.driver.page_source = "source"
+        self.driver.current_url = "http://current_url/"
+
+    def test_page(self):
+        page = Page(self.driver)
+        self.assertEqual(page.get_source(), "source")
+
+    def test_web_page(self):
+        page = WebPage(self.driver)
+        self.assertEqual(page.url, "http://current_url/")
+
+
