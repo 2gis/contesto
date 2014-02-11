286d26004e0bf8190dadf9e442ad7bcefb89949d
shiron <p.stashevsky@2gis.ru>
2014-02-11 14:09:27 +0700

    add optional init element in component

--- contesto/basis/component.py
+++ contesto/basis/component.py
@@ -5,7 +5,7 @@ from contesto.utils.log import trace
 class BaseComponent(object):
     __metaclass__ = AutoExtendingSelectors
 
-    def __init__(self, driver, element):
+    def __init__(self, driver, element=None):
         """
         :type driver: ContestoDriver
         :type element: ContestoWebElement
