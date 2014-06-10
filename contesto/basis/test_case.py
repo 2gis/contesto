64b905c59b64d11da27e898fb39838abee89a490
Nick Abalov <n.abalov@2gis.ru>
2014-06-10 16:55:33 +0700

    Fixed bug preventing prefix from being set to empty string

--- contesto/basis/test_case.py
+++ contesto/basis/test_case.py
@@ -10,7 +10,6 @@ from .driver_mixin import HttpDriver
 
 
 class ContestoTestCase(object):
-
     @classmethod
     def _setup_class(cls):
         if config.session["shared"]:
@@ -53,17 +52,19 @@ class ContestoTestCase(object):
 
         host = cls.driver_settings["host"]
         port = cls.driver_settings["port"]
-        if hasattr(cls.driver_settings, "prefix"):
-            prefix = cls.driver_settings["prefix"]
+        if "prefix" in cls.driver_settings.keys():
+            prefix = cls.driver_settings["prefix"].strip()
+            if len(prefix) > 0 and not prefix.startswith('/'):
+                prefix = '/' + prefix
         else:
-            prefix = 'wd/hub'
+            prefix = '/wd/hub'
 
         return cls._start_driver(desired_capabilities, host, port, prefix)
 
     @staticmethod
     def _start_driver(desired_capabilities, host, port, prefix):
         try:
-            command_executor = "http://%s:%d/%s" % (host, port, prefix)
+            command_executor = "http://%s:%d%s" % (host, port, prefix)
             return ContestoDriver(command_executor=command_executor, desired_capabilities=desired_capabilities)
         except URLError:
             raise ConnectionError(command_executor)
