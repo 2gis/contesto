0691fa41b7b89e75790c01e6ac6ed11996cf6f27
Igor Pavlov <nwlunatic@yandex.ru>
2015-07-20 14:45:19 +0600

    fix tests

--- tests/desired_capabilities_tests.py
+++ tests/desired_capabilities_tests.py
@@ -1,4 +1,5 @@
-from contesto.basis.driver_mixin import HttpDriver, IosDriver, QtWebkitDriver
+from contesto.basis.driver_mixin import SeleniumDriverMixin,\
+    IosDriverMixin, QtWebkitDriverMixin
 from contesto import config
 from contesto.basis.test_case import ContestoTestCase
 
@@ -11,14 +12,14 @@ class DesireCapabilitiesTestCase(unittest.TestCase):
         config.add_config_file(os.path.abspath(os.path.dirname(__file__)) + "/data/config/drivers.ini")
 
     def test_dc_loading_any_additional_setting(self):
-        driver = HttpDriver()
+        driver = SeleniumDriverMixin()
         driver_settings = getattr(config, driver._driver_type)
         desired_capabilities = driver._form_desired_capabilities(driver_settings)
         self.assertEqual(desired_capabilities.get('additional_setting'), 'some_value',
                          'should load any setting to desired_capabilities')
 
     def test_ios_driver(self):
-        driver = IosDriver()
+        driver = IosDriverMixin()
         driver_settings = getattr(config, driver._driver_type)
         desired_capabilities = driver._form_desired_capabilities(driver_settings)
         self.assertEqual(desired_capabilities["device"], "iPhone Simulator", 'wrong device in capabilities in iosdriver')
@@ -27,7 +28,7 @@ class DesireCapabilitiesTestCase(unittest.TestCase):
         self.assertEqual(desired_capabilities["version"], 7.0, 'wrong version in capabilities in iosdriver')
 
     def test_http_driver(self):
-        driver = HttpDriver()
+        driver = SeleniumDriverMixin()
         driver_settings = getattr(config, driver._driver_type)
         desired_capabilities = driver._form_desired_capabilities(driver_settings)
         self.assertEqual(desired_capabilities["browserName"], "firefox", 'wrong browser in capabilities in httpdriver')
@@ -35,7 +36,7 @@ class DesireCapabilitiesTestCase(unittest.TestCase):
         self.assertEqual(desired_capabilities["platform"], "ANY", 'wrong platform in capabilities in httpdriver')
 
     def test_qtwebkit_driver(self):
-        driver = QtWebkitDriver()
+        driver = QtWebkitDriverMixin()
         driver_settings = getattr(config, driver._driver_type)
         desired_capabilities = driver._form_desired_capabilities(driver_settings)
         self.assertEqual(desired_capabilities["app"], "/Users/test/app", 'wrong platform in capabilities in qtwebkitdriver')
@@ -50,7 +51,7 @@ class DictionaryDesireCapabilitiesTestCase(unittest.TestCase):
             "browserName": "firefox",
             "platform": "LINUX"
         }
-        driver = HttpDriver()
+        driver = SeleniumDriverMixin()
         driver_settings = getattr(config, driver._driver_type)
         desired_capabilities = driver._form_desired_capabilities(driver_settings)
         self.assertEqual(desired_capabilities, dc)
--- tests/proxy_tests.py
+++ tests/proxy_tests.py
@@ -5,7 +5,7 @@ import unittest
 
 from contesto.basis import test_case
 from contesto.basis.test_case import ContestoTestCase
-from contesto.basis.driver_mixin import HttpDriver
+from contesto.basis.driver_mixin import SeleniumDriverMixin
 
 
 class ProxyInDesireCapabilitiesTestCase(unittest.TestCase):
@@ -15,7 +15,7 @@ class ProxyInDesireCapabilitiesTestCase(unittest.TestCase):
         config.add_config_file(os.path.abspath(os.path.dirname(__file__)) + "/data/config/dc_with_proxy.ini")
 
     def test_proxy_adding_in_desired_capabilities_with_config(self):
-        driver = HttpDriver()
+        driver = SeleniumDriverMixin()
         driver_settings = getattr(config, driver._driver_type)
         desired_capabilities = driver._form_desired_capabilities(driver_settings)
         ContestoTestCase.desired_capabilities = desired_capabilities
@@ -40,7 +40,7 @@ class ProxyDisabledInDesireCapabilitiesTestCase(unittest.TestCase):
     proxy_host = "http://localhost:9091"
 
     def test_proxy_disabled_in_desired_capabilities_by_default(self):
-        driver = HttpDriver()
+        driver = SeleniumDriverMixin()
         driver_settings = getattr(config, driver._driver_type)
         desired_capabilities = driver._form_desired_capabilities(driver_settings)
         ContestoTestCase.desired_capabilities = desired_capabilities
