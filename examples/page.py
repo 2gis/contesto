b68b735cf1e5c29419de834924165e40c6b30619
Igor Pavlov <nwlunatic@yandex.ru>
2015-12-18 15:26:10 +0600

    add by locator helper

--- examples/page.py
+++ examples/page.py
@@ -1,8 +1,8 @@
-from contesto import Page, Component, find_element, by_class_name, by_css_selector
+from contesto import Page, Component, find_element, by
 
 
 class SearchResults(Component):
-    _header = by_class_name("mixedResults__header")
+    _header = by.class_name("mixedResults__header")
 
     def header(self):
         return find_element(self.element, self._header)
@@ -14,8 +14,8 @@ class SearchResults(Component):
 
 
 class SearchBar(Component):
-    _search_field = by_css_selector(".searchBar__textfield._directory .suggest__input")
-    _search_submit_button = by_css_selector(".searchBar__submit._directory")
+    _search_field = by.css_selector(".searchBar__textfield._directory .suggest__input")
+    _search_submit_button = by.css_selector(".searchBar__submit._directory")
 
     def __init__(self, driver, element):
         self.register_onload_elements([
@@ -36,8 +36,8 @@ class SearchBar(Component):
 
 
 class CityPage(Page):
-    _search_bar = by_class_name("online__searchBar")
-    _search_results = by_class_name("mixedResults")
+    _search_bar = by.class_name("online__searchBar")
+    _search_results = by.class_name("mixedResults")
 
     def __init__(self, driver):
         self.register_onload_elements([self._search_bar])
