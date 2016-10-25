b5179b47ac84319b87da8dce36466909ef120ae7
Розов Арсений Николаевич <a.rozov@2gis.ru>
2016-10-25 11:30:16 +0700

    remove stf-utils from requirements, add installation line in docs/utils.rst

--- setup.py
+++ setup.py
@@ -16,10 +16,6 @@ setup_args = {
         "Appium-Python-Client==0.20",
         "browsermob-proxy==0.7.1",
         "werkzeug==0.11.4",
-        "stf-utils==0.0.1",
-    ],
-    "dependency_links": [
-        "git+https://github.com/2gis/stf-utils.git@a9e85eeea8003673b200e9706a6a3ce079ddb7ca#egg=stf-utils-0.0.1"
     ],
     "license": "MIT",
     "description": "",
