5ee264ec917fb58690905c16ee417dbc95a9108a
Nikolay Ustinov <nikolay.ustinov90@gmail.com>
2019-01-16 11:51:12 +0700

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup_args = {
             'config/*.ini'
         ],
     },
-    "version": "0.3.3",
+    "version": "0.3.4",
     "install_requires": [
         "selenium==2.52.0",
         "Appium-Python-Client==0.24",
